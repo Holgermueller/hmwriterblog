ce8bdebbb5a53082f088f23b7944f6062b82275b
Holger Mueller <9jholger@gmail.com>
2020-04-27 14:29:38 -0500

    worked on contact form

---
+++ pages/thanks/thanks.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="thanks">
+    <v-card tile>
+      <v-card-title>
+        Thanks for dropping us a line!
+      </v-card-title>
+
+      <v-card-text>We'll get back to you as soon as possible! </v-card-text>
+
+      <v-card-actions>
+        <v-btn <nuxt-link to="../../contact"></nuxt-link> block>Back</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Thanks'
+}
+</script>
